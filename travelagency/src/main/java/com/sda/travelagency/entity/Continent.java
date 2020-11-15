08b68325440f336146701bf0c110f77dff2ed9bc
BobocelAdrian <65110531+BobocelAdrian@users.noreply.github.com>
2020-11-15 15:07:34 +0200

    Add files via upload
    update

---
+++ travelagency/src/main/java/com/sda/travelagency/entity/Continent.java
@@ -0,0 +1,74 @@
+package com.sda.travelagency.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Continent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String IsoCode;
+    private String description;
+    private String name;
+    private String rating;
+
+    public Continent() {
+    }
+
+
+    public Continent(Long id, String isoCode, String description, String name, String rating) {
+        this.id = id;
+        IsoCode = isoCode;
+        this.description = description;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIsoCode() {
+        return IsoCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setIsoCode(String isoCode) {
+        IsoCode = isoCode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
+
+
+
+
