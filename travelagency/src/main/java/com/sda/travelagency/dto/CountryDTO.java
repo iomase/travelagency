605682749cd0b1419ffecc518b90106685297020
BobocelAdrian <65110531+BobocelAdrian@users.noreply.github.com>
2020-11-15 15:05:55 +0200

    Add files via upload
    update

---
+++ travelagency/src/main/java/com/sda/travelagency/dto/CountryDTO.java
@@ -0,0 +1,49 @@
+package com.sda.travelagency.dto;
+
+public class CountryDTO {
+    private Long id;
+    private String IsoCode;
+    private String description;
+    private String name;
+    private String rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIsoCode() {
+        return IsoCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setIsoCode(String isoCode) {
+        IsoCode = isoCode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
