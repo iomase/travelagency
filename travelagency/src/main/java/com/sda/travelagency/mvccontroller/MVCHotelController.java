4ef9cf8f56b0ce5a1649c2d33965bcd156a71fea
Iomase <ionescumarianserban@gmail.com>
2020-11-22 23:48:49 +0200

    no message

--- travelagency/src/main/java/com/sda/travelagency/mvccontroller/MVCHotelController.java
+++ travelagency/src/main/java/com/sda/travelagency/mvccontroller/MVCHotelController.java
@@ -1,17 +1,27 @@
 package com.sda.travelagency.mvccontroller;
 
 import com.sda.travelagency.dto.HotelDTO;
+import com.sda.travelagency.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
 @Controller
 public class MVCHotelController {
 
+    @Autowired
+    private HotelService hotelService;
+
     @GetMapping("/viewHotels")
-    public String viewHotels() {
+    public String viewHotels(Model model) {
+        List<HotelDTO> hotelList = hotelService.getHotels();
+        model.addAttribute("hotels", hotelList);
         return "hotels-list";
     }
 
