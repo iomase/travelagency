f2cde7616b6a4771c1dfcda922c5db79d83e3a8a
Iomase <ionescumarianserban@gmail.com>
2020-12-20 22:30:50 +0200

    no message

---
+++ travelagency/src/main/java/com/sda/travelagency/TravelAgencyApplication.java
@@ -0,0 +1,12 @@
+package com.sda.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyApplication.class, args);
+    }
+}
